0439fb0cb97eab8e8a7c0404ce5016a332bfc18e
Kevin Duran Rojas <kevin101093@hotmail.com>
2026-05-20 15:25:41 -0600

    Integración de cropper-dialog.component y agregar una clase mat-elevation-z5 en el componente image.

--- src/app/supercop/cropper-dialog/cropper-dialog.component.ts
+++ src/app/supercop/cropper-dialog/cropper-dialog.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import {ImageCropperComponent} from 'ngx-image-cropper';
+import { MatButton } from "@angular/material/button";
+//import { } from
+
+export type CropperDialogData = {
+  image: File;
+  width: number;
+  height: number;
+}
+
+export type CropperDialogResult = {
+  blob: Blob;
+  imageUrl: string;
+}
 
 @Component({
+
   selector: 'app-cropper-dialog',
-  imports: [],
+  imports: [ImageCropperComponent, MatDialogModule, MatButton],
   templateUrl: './cropper-dialog.component.html',
-  styleUrl: './cropper-dialog.component.css'
+  styleUrls: ['./cropper-dialog.component.css']
 })
 export class CropperDialogComponent {
+  data: CropperDialogData = inject(MAT_DIALOG_DATA);
 
+  result = signal<CropperDialogResult | undefined>(undefined);
 }
